e61844fa60719de43570f9a0336272318b671fd6
Oleksandr Parkhomenko <oleksandr.parhomenko@yahoo.com>
2018-09-16 18:02:04 +0300

    Init

--- flask/car_search_run.py
+++ flask/car_search_run.py
@@ -1,32 +1,94 @@
+import re
 from flask import Flask, render_template, request, redirect, url_for
 from flask_material import Material 
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from flask_wtf.file import FileField
-from wtforms import TextField, HiddenField, ValidationError, RadioField,\
+from wtforms import StringField, HiddenField, ValidationError, RadioField,\
     BooleanField, SubmitField, IntegerField, FormField, validators
 from wtforms.validators import Required
 from flask_pymongo import PyMongo
 
+#from token import generate_confirmation_token, confirm_token
+#from email import send_email
+from flask_mail import Mail, Message
+from itsdangerous import URLSafeTimedSerializer
+
 app = Flask(__name__)  
 Material(app)  
 app.config['SECRET_KEY'] = 'USE-YOUR-OWN-SECRET-KEY-DAMNIT'
+app.config['SECURITY_PASSWORD_SALT'] = 'SALT'
 app.config['RECAPTCHA_PUBLIC_KEY'] = 'TEST'
+#app.config['MAIL_DEFAULT_SENDER'] = 'sender'
 app.config["MONGO_URI"] = "mongodb://localhost:27017/autoria"
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USERNAME'] = 'shoplab7@gmail.com'
+app.config['MAIL_PASSWORD'] = 'shoplab7test'
+app.config['MAIL_DEFAULT_SENDER'] = 'shoplab7@gmail.com'
 mongo = PyMongo(app)
+mail = Mail(app)
 
-class ExampleForm(Form):
-	mark_name = TextField('Mark', validators = [validators.required()])
-	model_name = TextField('Model')	  
+
+
+
+
+class ExampleForm(FlaskForm):
+	mark_name = StringField('Mark', validators = [validators.required()])
+	model_name = StringField('Model')
 			
-	submit_button = SubmitField('Find me a car!')	
+	submit_button = SubmitField('Find me a car!')
 
-class LoginForm(Form):
-	login = TextField('Enter your login', [validators.required()])
-	password = TextField('Enter your password', [validators.required()])
+class LoginForm(FlaskForm):
+	login = StringField('Enter your login', [validators.required()])
+	password = StringField('Enter your password', [validators.required()])
 	
-	#remember_me = BooleanField('remember_me')
 	
+class SignUpForm(FlaskForm):
+	login = StringField('Enter login', [validators.required()])
+	password = StringField('Enter password', [validators.required()])
+	confirm = StringField('Confirm password', [validators.required()])
+	name = StringField('Name', [validators.required()])
+	surname = StringField('Surname', [validators. required()])
+	email = StringField('E-mail', [validators. required()])
+	submit_button = SubmitField('Sign Up')
+	
+#GAVNOGAVNOGAVNO
+#GAVNOGAVNOGAVNO	
+#gavno
+#GAVNOGAVNOGAVNO	
+#gavno
 
+def send_email(to, subject, template):
+	msg = Message(
+		subject,
+		sender="TELETSKIYHUY@gmail.com",
+		recipients=[to],
+		html=template
+	)
+	mail.send(msg)
+	
+def generate_confirmation_token(email):
+	serializer = URLSafeTimedSerializer(app.config['SECRET_KEY'])
+	return serializer.dumps(email, salt=app.config['SECURITY_PASSWORD_SALT'])
+
+
+def confirm_token(token, expiration=3600):
+	serializer = URLSafeTimedSerializer(app.config['SECRET_KEY'])
+	email = serializer.loads(
+			token,
+			salt=app.config['SECURITY_PASSWORD_SALT'],
+			max_age=expiration
+		)
+	
+		#return False
+	return email
+
+#GAVNOGAVNOGAVNO	
+#gavno
+#GAVNOGAVNOGAVNO	
+#gavno
+	
 @app.route('/')  
 def welcome():
 	form = LoginForm()
@@ -42,7 +104,8 @@ def start():
 	form = ExampleForm(request.form)
 	
 	if form.validate_on_submit():
-		cars = list(mongo.db.new_cars.find( {"mark_name" : form.mark_name.data }))
+		regx = re.compile("^%s$" %(form.mark_name.data), re.IGNORECASE)
+		cars = list(mongo.db.new_cars.find( {"mark_name" : regx }))
 		return render_template('check.html', data = cars)
 	
 	return render_template('start.html', form = form)
@@ -53,13 +116,22 @@ def checkDB():
 		
 	return render_template('check.html', data = cars)
 
-@app.route('/register', methods = ['GET', 'POST'])
-def register():
-	
-	form = LoginForm()
+@app.route('/signup', methods = ['GET', 'POST'])
+def sign_up():
+	form = SignUpForm()
 	if form.validate_on_submit():
-		flash('Login requested for ' + form.login.data + '", remember_me=' + str(form.remember_me.data))
-		return redirect('/index')
-	return render_template('register.html', 
-		title = 'Sign In',
-		form = form)
+		token = generate_confirmation_token(form.email.data)
+	
+		confirm_url = url_for('welcome', _external=True) ##, token=token, _external=True)
+		html = render_template('activate.html', confirm_url=confirm_url)
+		subject = "Please confirm your email"
+		send_email(form.email.data, subject, html)
+		
+		return redirect('/start')
+		
+	return render_template('signUp.html', form = form)
+
+@app.route('/confirm/<token>')
+def confirm_email():
+	email = confirm_token(token)
+	return redirect('/')
---
+++ flask/token.py
@@ -0,0 +1,19 @@
+# token.py
+
+from itsdangerous import URLSafeTimedSerializer
+
+def generate_confirmation_token(email):
+	serializer = URLSafeTimedSerializer(app.config['SECRET_KEY'])
+	return serializer.dumps(email, salt=app.config['SECURITY_PASSWORD_SALT'])
+
+
+def confirm_token(token, expiration=3600):
+	serializer = URLSafeTimedSerializer(app.config['SECRET_KEY'])
+	email = serializer.loads(
+			token,
+			salt=app.config['SECURITY_PASSWORD_SALT'],
+			max_age=expiration
+		)
+	
+		#return False
+	return email
