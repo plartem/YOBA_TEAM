849decd69ca6ceaa5f741cd001e11dd138853dad
karlion <maksim.mykhaylenko@gmail.com>
2018-09-17 16:19:24 +0300

    avoid duplicates in db

--- crawler/crawler/pipelines.py
+++ crawler/crawler/pipelines.py
@@ -6,9 +6,9 @@
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 
-#class AutoriaCrawlerPipeline(object):
- #   def process_item(self, item, spider):
-  #      return item
+# class AutoriaCrawlerPipeline(object):
+#   def process_item(self, item, spider):
+#      return item
 
 import pymongo
 
@@ -16,26 +16,42 @@ from scrapy.conf import settings
 from scrapy.exceptions import DropItem
 from scrapy import log
 
+
 class MongoDBPipeline(object):
 
-	def __init__(self):
-		connection = pymongo.MongoClient(
-			settings['MONGODB_SERVER'],
-			settings['MONGODB_PORT']
-		)
-		db = connection[settings['MONGODB_DB']]
-		self.collection = db[settings['MONGODB_COLLECTION']]
+    def __init__(self):
+        connection = pymongo.MongoClient(
+            settings['MONGODB_SERVER'],
+            settings['MONGODB_PORT']
+        )
+        db = connection[settings['MONGODB_DB']]
+        self.collection = db[settings['MONGODB_COLLECTION']]
 
-	def process_item(self, item, spider):
-		valid = True
-		for data in item:
-			if not data:
-				valid = False
-				raise DropItem("Missing {0}!".format(data))
-		if valid:
-			self.collection.insert(dict(item))
-			log.msg("Car added to MongoDB database!",
-				level=log.DEBUG, spider=spider)
-		return item
-		
-		
+    def process_item(self, item, spider):
+        valid = True
+        for data in item:
+            if not data:
+                valid = False
+                raise DropItem("Missing {0}!".format(data))
+        if valid:
+            self.collection.update(
+                {"url": item["url"]},
+                {"$set":{
+                    "image": item["image"],
+                    "mark_name": item["mark_name"],
+                    "model_name": item["model_name"],
+                    "location": item["location"],
+                    "price": item["price"],
+                    "mileage": item["mileage"],
+                    "info": item["info"],
+                    "transmission": item["transmission"],
+                    "fuel": item["fuel"],
+                    "year": item["year"]
+                }},
+                upsert=True
+            )
+
+            #self.collection.insert(dict(item))
+            log.msg("Car added to MongoDB database!",
+                    level=log.DEBUG, spider=spider)
+        return item
