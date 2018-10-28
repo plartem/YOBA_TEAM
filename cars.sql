3c16643b506bb9d857aa36d5fbd8baf801fefee0
Maxim <maxim1998g@gmail.com>
2018-10-28 23:07:50 +0200

    Update mysql database

--- cars.sql
+++ cars.sql
@@ -1,32 +1,10 @@
--- phpMyAdmin SQL Dump
--- version 4.7.0
--- https://www.phpmyadmin.net/
---
--- Хост: localhost
--- Время создания: Сен 16 2018 г., 23:17
--- Версия сервера: 5.7.18
--- Версия PHP: 7.1.5
+CREATE DATABASE IF NOT EXISTS `cars`;
+USE `cars`;
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- База данных: `cars`
---
-
--- --------------------------------------------------------
-
---
--- Структура таблицы `queries`
---
 
 CREATE TABLE `queries` (
   `id` int(11) NOT NULL,
@@ -36,21 +14,13 @@ CREATE TABLE `queries` (
   `low_price` int(11) NOT NULL,
   `year` int(11) NOT NULL,
   `mileage` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL
+  `user_id` int(11) NOT NULL,
+  `timeinterval` int(11) NOT NULL DEFAULT 15,
+  `updatedAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Дамп данных таблицы `queries`
---
 
 INSERT INTO `queries` (`id`, `mark`, `model`, `high_price`, `low_price`, `year`, `mileage`, `user_id`) VALUES
 (1, 'Ford', '', -1, 100, 2000, 10000, 9);
-
--- --------------------------------------------------------
-
---
--- Структура таблицы `users`
---
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
@@ -62,18 +32,8 @@ CREATE TABLE `users` (
   `token` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Дамп данных таблицы `users`
---
-
 INSERT INTO `users` (`id`, `email`, `password`, `name`, `surname`, `login`, `token`) VALUES
 (9, 'bcklmnop@gmail.com', '$2b$12$/2AI2MgRVQ9zirUKIZkKOOUx7V1r2thW2p5xcFc4rRbbpYhHf5evy', 'bcklmnop', 'bcklmnop', 'bcklmnop', '');
-
--- --------------------------------------------------------
-
---
--- Структура таблицы `logs`
---
 
 CREATE TABLE IF NOT EXISTS `logs` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -84,19 +44,9 @@ CREATE TABLE IF NOT EXISTS `logs` (
   UNIQUE KEY `id` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
---
--- Индексы сохранённых таблиц
---
-
---
--- Индексы таблицы `queries`
---
 ALTER TABLE `queries`
   ADD PRIMARY KEY (`id`);
 
---
--- Индексы таблицы `users`
---
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `email` (`email`),
@@ -104,21 +54,8 @@ ALTER TABLE `users`
   ADD KEY `id` (`id`),
   ADD KEY `id_2` (`id`);
 
---
--- AUTO_INCREMENT для сохранённых таблиц
---
-
---
--- AUTO_INCREMENT для таблицы `queries`
---
 ALTER TABLE `queries`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
---
--- AUTO_INCREMENT для таблицы `users`
---
+
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
